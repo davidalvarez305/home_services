c44210d1b6cc5d1352ba38818e74e227dfd62074
david alvarez <david@southfloridaathleticclub.com>
2022-10-31 22:23:00 -0400

    next env

--- crm/src/constants.ts
+++ crm/src/constants.ts
@@ -1,5 +1,5 @@
-export const LOGIN_ROUTE = process.env.API_URL + "api/user/login";
-export const LOGOUT_ROUTE = process.env.API_URL + "api/user/logout";
-export const USER_ROUTE = process.env.API_URL + "api/user";
-export const CLIENT_DOMAIN = process.env.CLIENT_DOMAIN;
-export const BUCKET_URL = process.env.BUCKET_URL;
+export const LOGIN_ROUTE = process.env.NEXT_PUBLIC_API_URL + "api/user/login";
+export const LOGOUT_ROUTE = process.env.NEXT_PUBLIC_API_URL + "api/user/logout";
+export const USER_ROUTE = process.env.NEXT_PUBLIC_API_URL + "api/user";
+export const CLIENT_DOMAIN = process.env.NEXT_PUBLIC_CLIENT_DOMAIN;
+export const BUCKET_URL = process.env.NEXT_PUBLIC_BUCKET_URL;
