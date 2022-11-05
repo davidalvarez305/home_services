08e018f14010ea101f2d7754427e459036b7e72f
david alvarez <david@southfloridaathleticclub.com>
2022-11-05 02:36:44 -0400

    company account status

--- server/models/company_account_status.go
+++ server/models/company_account_status.go
@@ -3,7 +3,7 @@ package models
 type CompanyAccountStatus struct {
 	ID              int            `json:"id"`
 	CompanyID       int            `json:"company_id"`
-	Company         *Company       `gorm:"not null;column:company_id;foreignKey:CompanyID;constraint:OnDelete:CASCADE,OnUpdate:CASCADE" json:"-"`
+	Company         *Company       `gorm:"unique;not null;column:company_id;foreignKey:CompanyID;constraint:OnDelete:CASCADE,OnUpdate:CASCADE" json:"-"`
 	AccountStatusID int            `json:"account_status_id"`
 	AccountStatus   *AccountStatus `gorm:"not null;column:account_status_id;foreignKey:AccountStatusID;constraint:OnDelete:CASCADE,OnUpdate:CASCADE" json:"-"`
 	CreatedAt       int64          `gorm:"not null;column:created_at" json:"created_at"`
