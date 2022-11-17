e61231addfbacfd615ede6a4c6f171cf62434bd0
david alvarez <david@southfloridaathleticclub.com>
2022-11-16 23:29:39 -0500

    unique composite index

--- server/models/company_services_locations.go
+++ server/models/company_services_locations.go
@@ -3,9 +3,9 @@ package models
 type CompanyServicesLocations struct {
 	ID        int      `json:"id"`
 	ServiceID int      `json:"service_id"`
-	Service   *Service `gorm:"not null;column:service_id;foreignKey:ServiceID;constraint:OnDelete:CASCADE,OnUpdate:CASCADE" json:"-"`
+	Service   *Service `gorm:"uniqueIndex:compositeindex;not null;column:service_id;foreignKey:ServiceID;constraint:OnDelete:CASCADE,OnUpdate:CASCADE" json:"-"`
 	CompanyID int      `json:"company_id"`
-	Company   *Company `gorm:"not null;column:company_id;foreignKey:CompanyID;constraint:OnDelete:CASCADE,OnUpdate:CASCADE" json:"-"`
+	Company   *Company `gorm:"uniqueIndex:compositeindex;not null;column:company_id;foreignKey:CompanyID;constraint:OnDelete:CASCADE,OnUpdate:CASCADE" json:"-"`
 	ZipCodeID int      `json:"zip_code_id"`
-	ZipCode   *ZipCode `gorm:"not null;column:zip_code_id;foreignKey:ZipCodeID;constraint:OnDelete:CASCADE,OnUpdate:CASCADE" json:"-"`
+	ZipCode   *ZipCode `gorm:"uniqueIndex:compositeindex;not null;column:zip_code_id;foreignKey:ZipCodeID;constraint:OnDelete:CASCADE,OnUpdate:CASCADE" json:"-"`
 }
