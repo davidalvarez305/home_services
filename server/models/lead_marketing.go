7905046e0df04bbab29b72dcf421b4ffef9832a2
david alvarez <david@southfloridaathleticclub.com>
2022-11-17 23:47:40 -0500

    lead marketing

--- server/models/lead_marketing.go
+++ server/models/lead_marketing.go
@@ -5,7 +5,9 @@ type LeadMarketing struct {
 	Lead         *Lead  `gorm:"not null;column:lead_id;foreignKey:LeadID;constraint:OnDelete:CASCADE,OnUpdate:CASCADE" json:"-"`
 	Campaign     string `gorm:"unique;not null" json:"campaign"`
 	Source       string `json:"source"`
+	Medium       string `json:"medium"`
 	CampaignName string `json:"campaign_name"`
+	LeadChannel  string `json:"lead_channel"`
 	ReferralURL  string `json:"referral_url"`
 	Keywords     string `json:"keywords"`
 }
