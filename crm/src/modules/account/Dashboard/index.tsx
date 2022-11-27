f9e22725d416ae9eee03185f25745fc390699cba
david alvarez <david@southfloridaathleticclub.com>
2022-11-27 16:43:43 -0500

    empty large box component

--- crm/src/modules/account/Dashboard/index.tsx
+++ crm/src/modules/account/Dashboard/index.tsx
@@ -2,20 +2,35 @@ import React from "react";
 import PrimaryLayout from "../../../layout/Primary";
 import useLeadAuth from "../../../hooks/useLeadAuth";
 import LargeBox from "../../../components/LargeBox";
-
+import EmptyLargeBox from "../../../components/EmptyLargeBox";
+import styles from "./Dashboard.module.css";
+import { IconButton } from "@chakra-ui/react";
+import { GoPlus } from "react-icons/go";
 const Dashboard: React.FC = () => {
   useLeadAuth();
 
   return (
     <PrimaryLayout screenName="Dashboard">
-      <LargeBox
-        bottomLeftHeader={""}
-        bottomLeftRegularParagraph={"Hialeah"}
-        bottomLeftBoldedParagraph={"FL"}
-        bottomRightHeader={"Bathroom Remodeling"}
-        topLeftHeader={"Budget Amount"}
-        topLeftRegularParagraph={'$5000'}
-      />
+      <div className={styles["main-container"]}>
+        <LargeBox
+          bottomLeftHeader={""}
+          bottomLeftRegularParagraph={"Hialeah"}
+          bottomLeftBoldedParagraph={"FL"}
+          bottomRightHeader={"Bathroom Remodeling"}
+          topLeftHeader={"Budget Amount"}
+          topLeftRegularParagraph={"$5000"}
+        />
+        <EmptyLargeBox>
+          <IconButton
+            colorScheme={"blue"}
+            variant={"ghost"}
+            fontSize={"50px"}
+            size={"lg"}
+            aria-label="add"
+            icon={<GoPlus />}
+          />
+        </EmptyLargeBox>
+      </div>
     </PrimaryLayout>
   );
 };
