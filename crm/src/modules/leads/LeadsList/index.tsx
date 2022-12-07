248aa104e979748376562dd0f58dab23335ae9b0
david alvarez <david@southfloridaathleticclub.com>
2022-12-07 03:30:29 -0500

    load more logic

--- crm/src/modules/leads/LeadsList/index.tsx
+++ crm/src/modules/leads/LeadsList/index.tsx
@@ -7,33 +7,56 @@ import { COMPANY_ROUTE } from "../../../constants";
 import { CompanyLead } from "../../../types/general";
 import LeadsTable from "../../../components/LeadsTable";
 import styles from "./LeadsList.module.css";
+import Button from "../../../components/Button";
 
 const LeadsList: React.FC = () => {
   const ctx = useContext(UserContext);
   useLoginRequired();
   const { makeRequest, isLoading, error } = useFetch();
   const [companyLeads, setCompanyLeads] = useState<CompanyLead[]>([]);
+  const [offsetLimits, setOffsetLimits] = useState({
+    offset: 0,
+    limit: 8,
+  });
 
   useEffect(() => {
     if (ctx?.user.company_id) {
       const qs = new URLSearchParams({
-        offset: "0",
-        limit: "5",
+        offset: String(offsetLimits.offset),
+        limit: String(offsetLimits.limit),
       });
 
       makeRequest(
         { url: `${COMPANY_ROUTE}/${ctx?.user.company_id}/leads/?` + qs },
         (res) => {
-          setCompanyLeads(res.data.data);
+          setCompanyLeads((prev) => [...prev, ...res.data.data]);
         }
       );
     }
-  }, [makeRequest, ctx?.user.company_id]);
+  }, [makeRequest, ctx?.user.company_id, offsetLimits]);
+
+  function handleLoadMore() {
+    setOffsetLimits((prev) => {
+      return {
+        offset: (prev.offset += prev.limit),
+        limit: 8,
+      };
+    });
+  }
 
   return (
     <PrimaryLayout screenName="Leads List">
-      <div className={styles['main-container']}>
-      {companyLeads.length > 0 && <LeadsTable companyLeads={companyLeads} />}
+      <div className={styles["main-container"]}>
+        <div className={styles["table-button"]}>
+          <LeadsTable companyLeads={companyLeads} />
+          <Button
+            className="Dark"
+            isLoading={isLoading}
+            onClick={() => handleLoadMore()}
+          >
+            Load More
+          </Button>
+        </div>
       </div>
     </PrimaryLayout>
   );
