931c77404e6d9b0c4c1ce24daeaac544b5c40aa0
david alvarez <david@southfloridaathleticclub.com>
2022-12-08 05:04:27 -0500

    working on stripe invoices

--- server/actions/stripe.go
+++ server/actions/stripe.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stripe/stripe-go/v74"
 	"github.com/stripe/stripe-go/v74/customer"
+	"github.com/stripe/stripe-go/v74/invoice"
 )
 
 func (company *Company) CreateStripeCustomer(owner *User) error {
@@ -28,4 +29,36 @@ func (company *Company) CreateStripeCustomer(owner *User) error {
 	company.StripeCustomerID = c.ID
 
 	return company.Save()
+}
+
+func CreateInvoice() error {
+	stripe.Key = os.Getenv("STRIPE_SECRET_KEY")
+
+	customFields := []*stripe.InvoiceCustomFieldParams{}
+
+	customFields = append(customFields, &stripe.InvoiceCustomFieldParams{
+		Name:  stripe.String("Number of Leads Delivered"),
+		Value: stripe.String("99"),
+	})
+
+	params := &stripe.InvoiceParams{
+		Customer:             stripe.String(COMPANY_ID),
+		AutoAdvance:          stripe.Bool(true),
+		CollectionMethod:     stripe.String("send_invoice"),
+		Currency:             stripe.String("USD"),
+		DaysUntilDue:         stripe.Int64(30),
+		CustomFields:         customFields,
+		DefaultPaymentMethod: stripe.String(),
+		Description:          stripe.String(),
+		DueDate:              stripe.String(),
+		OnBehalfOf:           stripe.String(),
+	}
+
+	in, err := invoice.New(params)
+
+	if err != nil {
+		return err
+	}
+
+	in.AmountDue
 }
