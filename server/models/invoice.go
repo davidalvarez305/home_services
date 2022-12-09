2905d7f4c468f8fcae20f34ae451941481f215cc
david alvarez <david@southfloridaathleticclub.com>
2022-12-09 00:40:43 -0500

    invoice

--- server/models/invoice.go
+++ server/models/invoice.go
@@ -2,9 +2,12 @@ package models
 
 type Invoice struct {
 	ID                     int            `json:"id"`
+	InvoiceID              string         `gorm:"not null;column:invoice_id" json:"invoice_id"`
 	InvoiceAmount          int            `gorm:"not null;column:invoice_amount" json:"invoice_amount"`
 	InvoiceDueDate         int64          `gorm:"not null;column:invoice_due_date" json:"invoice_due_date"`
 	InvoicePaymentStatusID int            `json:"invoice_payment_status_id"`
-	PaymentStatus          *PaymentStatus `gorm:"not null;column:invoice_payment_status_id;foreignKey:InvoicePaymentStatusID;constraint:OnDelete:CASCADE,OnUpdate:CASCADE" json:"-"`
+	PaymentStatus          *PaymentStatus `gorm:"not null;column:invoice_payment_status_id;foreignKey:InvoicePaymentStatusID;CASCADE,OnUpdate:CASCADE" json:"-"`
+	CompanyID              int            `json:"company_id"`
+	Company                *Company       `gorm:"not null;column:company_id;foreignKey:CompanyID;constraint:OnUpdate:CASCADE" json:"-"`
 	Lead                   []*Lead        `gorm:"many2many:invoice_leads" json:"-"`
 }
