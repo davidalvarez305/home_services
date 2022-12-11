f722d0aa2c29bf20ad3d0f1cdb31e9618e55b2df
david alvarez <david@southfloridaathleticclub.com>
2022-12-11 03:10:09 -0500

    invoice payment failed

--- server/handlers/stripe.go
+++ server/handlers/stripe.go
@@ -3,8 +3,10 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/davidalvarez305/home_services/server/actions"
+	"github.com/davidalvarez305/home_services/server/utils"
 	"github.com/gofiber/fiber/v2"
 	"github.com/stripe/stripe-go/v74"
 )
@@ -64,14 +66,44 @@ func HandleStripeWebhooks(c *fiber.Ctx) error {
 	case "invoice.payment_failed":
 		var invoice stripe.Invoice
 		err := json.Unmarshal(event.Data.Raw, &invoice)
+
 		if err != nil {
 			return c.Status(400).JSON(fiber.Map{
 				"data": "Could not parse payment method.",
 			})
 		}
 
+		// Fetch invoice
+		inv := &actions.Invoice{}
+
+		err = inv.GetInvoiceByInvoiceID(invoice.ID)
+
+		if err != nil {
+			return c.Status(400).JSON(fiber.Map{
+				"data": "Could not fetch invoice by invoice ID.",
+			})
+		}
+
 		// Create log
+		log := &actions.CompanyLog{}
+
+		err = log.Save("Invoice payment failed.", fmt.Sprintf("%+v", inv.CompanyID))
+
+		if err != nil {
+			return c.Status(400).JSON(fiber.Map{
+				"data": "Failed to save log.",
+			})
+		}
+
 		// E-mail to myself with details
+		msg := fmt.Sprintf("Invoice payment failed for %v", inv.CompanyID)
+		err = utils.SendGmail(msg, os.Getenv("GMAIL_EMAIL"), "Invoice payment failed.")
+
+		if err != nil {
+			return c.Status(400).JSON(fiber.Map{
+				"data": "Failed to send e-mail.",
+			})
+		}
 	}
 
 	return c.Status(200).JSON(fiber.Map{
