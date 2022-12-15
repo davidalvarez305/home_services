af83292f2aa6ccc76ffb1c4eeff23ea920344409
david alvarez <david@southfloridaathleticclub.com>
2022-12-15 02:21:37 -0500

    auth footer link

---
+++ crm/src/hooks/useAuthFooterLinks.tsx
@@ -0,0 +1,29 @@
+import { useRouter } from "next/router";
+import { useCallback, useEffect, useState } from "react";
+
+export default function useAuthFooterLinks() {
+  const [path, setPath] = useState("");
+  const [msg, setMsg] = useState({ top: "", bottom: "" });
+  const router = useRouter();
+
+  const redirect = useCallback(() => {
+    router.push(path);
+  }, [path, router]);
+
+  useEffect(() => {
+    const currentPage = window.location.href;
+
+    switch (true) {
+      case currentPage.includes("register"):
+        setPath("/login");
+        setMsg({ top: "Have an account?", bottom: "Sign in." });
+        break;
+      case currentPage.includes("login"):
+        setPath("/register");
+        setMsg({ top: "Don’t have an account yet?", bottom: "Create one." });
+        break;
+    }
+  }, []);
+
+  return { redirect, msg };
+}
