381fd7bcafa1449001cd411c747571771e66b4e5
david alvarez <david@southfloridaathleticclub.com>
2022-12-17 00:50:51 -0500

    small close icon button thing

--- crm/src/components/CustomModal.tsx
+++ crm/src/components/CustomModal.tsx
@@ -2,6 +2,7 @@ import { Dispatch, Fragment, SetStateAction } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import Slider from "react-slick";
 import Head from "next/head";
+import SmallCloseIcon from "../assets/SmallCloseIcon";
 
 type Props = {
   children: React.ReactNode;
@@ -80,18 +81,7 @@ export default function CustomModal({ children, setIsOpen, isOpen }:Props) {
                       className="inline-flex justify-center items-center space-x-2 border font-semibold focus:outline-none px-2 py-1 leading-5 text-sm rounded border-transparent text-gray-600 hover:text-gray-400 focus:ring focus:ring-gray-500 focus:ring-opacity-25 active:text-gray-600"
                       onClick={closeModal}
                     >
-                      <svg
-                        className="hi-solid hi-x inline-block w-4 h-4 -mx-1"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
+                      <SmallCloseIcon />
                     </button>
                   </div>
                 </div>
