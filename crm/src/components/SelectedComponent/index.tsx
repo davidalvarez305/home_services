fd9c645e9f3ca59e28ff89c36c20f0bde099e998
david alvarez <david@southfloridaathleticclub.com>
2023-01-02 00:44:02 -0500

    by mistake this actually looks beautiful

--- crm/src/components/SelectedComponent/index.tsx
+++ crm/src/components/SelectedComponent/index.tsx
@@ -1,35 +1,21 @@
 import { MouseEventHandler } from "react";
-import { Line } from "../../assets/Line";
-import DeleteButton from "../DeleteIconButton";
-import styles from "./SelectedComponent.module.css";
+import SmallXIcon from "../../assets/SmallXIcon";
 
 interface Props {
   selected: string;
-  onClick: MouseEventHandler<HTMLButtonElement>;
+  onClick: MouseEventHandler<HTMLDivElement>;
 }
 
 export const SelectedComponent: React.FC<Props> = ({ selected, onClick }) => {
   return (
-    <div className={styles["main-select-component"]}>
-      <div className={styles["flex-row"]}>
-        <div className={styles["header"]}>
-          <div className={styles["icon"]}>
-            <DeleteButton
-              size={"sm"}
-              width={9}
-              height={9}
-              aria-label={"removeUsers"}
-              className={styles["line"]}
-              onClick={onClick}
-            />
-          </div>
-          <div className={styles["label"]}>
-            <div className={styles["label-body"]}>{selected}</div>
-            <div className={styles["sub-header"]}>{"Florida"}</div>
-          </div>
-        </div>
-      </div>
-      <Line />
+    <div className="p-3 text-center" onClick={onClick}>
+      <button
+        type="button"
+        className="inline-flex justify-center items-center space-x-2 border font-semibold focus:outline-none px-2 py-1 leading-5 text-sm rounded border-gray-300 bg-white text-gray-800 shadow-sm hover:text-gray-800 hover:bg-gray-100 hover:border-gray-300 hover:shadow focus:ring focus:ring-gray-500 focus:ring-opacity-25 active:bg-white active:border-white active:shadow-none"
+      >
+        <SmallXIcon />
+        <span>{selected}</span>
+      </button>
     </div>
   );
 };
