c9937e32c209d42b32fca702f937cdfeb7e9edbb
david alvarez <david@southfloridaathleticclub.com>
2023-01-06 05:23:12 -0500

    download leads data

--- crm/src/components/InvoicesTable.tsx
+++ crm/src/components/InvoicesTable.tsx
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { Invoice } from "../types/general";
 import TableDateComponent from "./TableDateComponent";
+import { AiOutlineDownload } from "react-icons/ai";
+import Button from "./Button";
+import * as XLSX from "xlsx";
 
 interface Props {
   invoices: Invoice[];
 }
-export default function InvoicesTable({ invoices }:Props) {
-  console.log('invoices: ', invoices);
-    const INVOICE_TABLE_HEADERS = ["Invoice Amount", "Due Date", "Payment Status", "Qty. of Leads"]
+export default function InvoicesTable({ invoices }: Props) {
+  const INVOICE_TABLE_HEADERS = [
+    "Invoice Amount",
+    "Due Date",
+    "Payment Status",
+    "Qty. of Leads",
+    "Download",
+  ];
+
+  function handleDownloadFile(invoice: Invoice) {
+    const leads = invoice.leads.map(
+      ({ email, first_name, last_name, phone_number }) => {
+        return { email, first_name, last_name, phone_number };
+      }
+    );
+
+    const results = XLSX.utils.json_to_sheet(leads);
+    const path = `${invoice.invoice_id}.csv`;
+
+    var blob = new File([XLSX.utils.sheet_to_csv(results)], path, {
+      type: "text/csv",
+    });
+    var url = URL.createObjectURL(blob);
+
+    var a = document.createElement("a");
+    a.href = url;
+    a.download = path;
+
+    a.click();
+  }
+
   return (
     <div className="border border-gray-200 rounded overflow-x-auto min-w-full bg-white">
       <table className="min-w-full text-sm align-middle whitespace-nowrap">
@@ -20,7 +51,7 @@ export default function InvoicesTable({ invoices }:Props) {
                 className="p-3 text-gray-700 bg-gray-100 font-semibold text-sm tracking-wider uppercase text-center"
               >
                 <div className="flex justify-center items-center gap-1">
-                {header}
+                  {header}
                 </div>
               </th>
             ))}
@@ -29,22 +60,30 @@ export default function InvoicesTable({ invoices }:Props) {
 
         <tbody>
           {invoices.map((invoice, index) => (
-            <tr className={index % 2 !== 0 ? "bg-gray-50" : ""} key={invoice.invoice_id}>
+            <tr
+              className={index % 2 !== 0 ? "bg-gray-50" : ""}
+              key={invoice.invoice_id}
+            >
               <td key={uuidv4()} className="p-3 text-center">
-                <p className="font-medium">
-                    {invoice.invoice_amount}
-                </p>
+                <p className="font-medium">{invoice.invoice_amount}</p>
               </td>
               <TableDateComponent date={invoice.invoice_due_date} />
               <td key={uuidv4()} className="p-3 text-center">
                 <p className="font-medium">
-                    {invoice.invoice_payment_status_id}
+                  {invoice.invoice_payment_status_id}
                 </p>
               </td>
               <td key={uuidv4()} className="p-3 text-center">
-                <p className="font-medium">
-                    {`${invoice.leads.length} leads`}
-                </p>
+                <p className="font-medium">{`${invoice.leads.length} leads`}</p>
+              </td>
+              <td key={uuidv4()} className="p-3 text-center">
+                <Button onClick={() => handleDownloadFile(invoice)}>
+                  <AiOutlineDownload />
+                  <div
+                    style={{ display: "none" }}
+                    id={`${invoice.invoice_id}-json`}
+                  ></div>
+                </Button>
               </td>
             </tr>
           ))}
@@ -52,4 +91,4 @@ export default function InvoicesTable({ invoices }:Props) {
       </table>
     </div>
   );
-};
+}
