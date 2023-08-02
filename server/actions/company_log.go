1bc1c8c4170dea93b4eee083072706053f71e8be
david alvarez <david@southfloridaathleticclub.com>
2023-08-01 23:07:36 -0400

    company log actions

--- server/actions/company_log.go
+++ server/actions/company_log.go
@@ -8,29 +8,24 @@ import (
 	"github.com/davidalvarez305/home_services/server/models"
 )
 
-type CompanyLog struct {
-	*models.CompanyLog
-}
-
-type CompanyLogs []*models.CompanyLog
-
-func (l *CompanyLog) Save(action string, companyId string) error {
-
+func SaveCompanyLog(action string, companyId string) error {
 	company, err := strconv.Atoi(companyId)
 
 	if err != nil {
 		return err
 	}
 
-	l.CompanyLog = &models.CompanyLog{
+	log := models.CompanyLog{
 		Action:    action,
 		CreatedAt: time.Now().Unix(),
 		CompanyID: company,
 	}
 
-	return database.DB.Save(&l).Error
+	return database.DB.Save(&log).Error
 }
 
-func (logs *CompanyLogs) Get(companyId string) error {
-	return database.DB.Where("company_id = ?", companyId).Find(&logs).Error
+func GetCompanyLogs(companyId string) ([]models.CompanyLog, error) {
+	var logs []models.CompanyLog
+	err := database.DB.Where("company_id = ?", companyId).Find(&logs).Error
+	return logs, err
 }
