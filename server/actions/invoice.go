47145a3d97fa240ae0a80f61d9782432dda8e701
david alvarez <david@southfloridaathleticclub.com>
2023-08-26 00:17:58 -0400

    invoice actions

--- server/actions/invoice.go
+++ server/actions/invoice.go
@@ -5,25 +5,24 @@ import (
 	"github.com/davidalvarez305/home_services/server/models"
 )
 
-type Invoice struct {
-	*models.Invoice
+func SaveInvoice(invoice models.Invoice) error {
+	return database.DB.Save(&invoice).Association("InvoiceLeads").Error
 }
 
-type Invoices []*models.Invoice
-
-func (i *Invoice) Save() error {
-	return database.DB.Save(&i).Association("InvoiceLeads").Error
+func GetInvoiceByID(id string) (models.Invoice, error) {
+	var invoice models.Invoice
+	err := database.DB.Where("id = ?", id).Preload("Lead").First(&invoice).Error
+	return invoice, err
 }
 
-func (i *Invoice) GetInvoiceByID(id string) error {
-	return database.DB.Where("id = ?", id).Preload("Lead").First(&i).Error
+func GetCompanyInvoices(companyId string) ([]models.Invoice, error) {
+	var invoices []models.Invoice
+	err := database.DB.Where("company_id = ?", companyId).Preload("Lead.Address").Preload("Lead.Service").Preload("Company.Address.City").Preload("Company.Address.State").Preload("Company.Address.Country").Preload("PaymentStatus").Find(&invoices).Error
+	return invoices, err
 }
 
-func (i *Invoices) GetCompanyInvoices(companyId string) error {
-	err := database.DB.Where("company_id = ?", companyId).Preload("Lead.Address").Preload("Lead.Service").Preload("Company.Address.City").Preload("Company.Address.State").Preload("Company.Address.Country").Preload("PaymentStatus").Find(&i).Error
-	return err
-}
-
-func (i *Invoice) GetInvoiceByInvoiceID(invoice_id string) error {
-	return database.DB.Where("invoice_id = ?", invoice_id).First(&i).Error
+func GetInvoiceByInvoiceID(invoice_id string) (models.Invoice, error) {
+	var invoice models.Invoice
+	err := database.DB.Where("invoice_id = ?", invoice_id).First(&invoice).Error
+	return invoice, err
 }
