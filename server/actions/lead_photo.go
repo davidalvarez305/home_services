ed4b05055c55961c490b8daf590f7ddcfcd68040
david alvarez <david@southfloridaathleticclub.com>
2023-08-26 01:04:50 -0400

    lead photo actions

--- server/actions/lead_photo.go
+++ server/actions/lead_photo.go
@@ -5,32 +5,27 @@ import (
 	"github.com/davidalvarez305/home_services/server/models"
 )
 
-type LeadPhoto struct {
-	*models.LeadPhoto
-}
-
-type LeadPhotos []*models.LeadPhoto
-
 type LeadPhotoURL struct {
 	ImageURL string `json:"image_url"`
 }
 
-type PhotoURLs []*LeadPhotoURL
-
 // Save single Lead Photos
-func (l *LeadPhoto) Save() error {
-	return database.DB.Save(&l).First(&l).Error
+func SaveLeadPhoto(leadPhoto models.LeadPhoto) error {
+	return database.DB.Save(&leadPhoto).First(&leadPhoto).Error
 }
 
-func (l *LeadPhoto) DeleteLeadPhoto(imageUrl string) error {
-	return database.DB.Where("image_url = ?", imageUrl).Delete(&l).Error
+func DeleteLeadPhoto(imageUrl string) error {
+	var leadPhoto models.LeadPhoto
+	return database.DB.Where("image_url = ?", imageUrl).Find(&leadPhoto).Delete(&leadPhoto).Error
 }
 
-func (l *PhotoURLs) GetPhotoURLsByLeadID(leadId string) error {
-	return database.DB.Raw("SELECT image_url FROM lead_photo WHERE lead_id = ?", leadId).Scan(&l).Error
+func GetPhotoURLsByLeadID(leadId string) ([]LeadPhotoURL, error) {
+	var photoUrls []LeadPhotoURL
+	err := database.DB.Raw("SELECT image_url FROM lead_photo WHERE lead_id = ?", leadId).Scan(&photoUrls).Error
+	return photoUrls, err
 }
 
 // Save multiple Lead Photos
-func (l *LeadPhotos) Save() error {
-	return database.DB.Save(&l).Find(&l).Error
+func SaveLeadPhotos(leadPhotos []models.LeadPhoto) error {
+	return database.DB.Save(&leadPhotos).Find(&leadPhotos).Error
 }
