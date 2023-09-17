9753abcd78ae18d29f8c21a2395214b4a1f1bc8a
david alvarez <david@southfloridaathleticclub.com>
2023-09-17 01:04:16 -0400

    APPLIED MIDDLEWARE

--- server/controllers/company.go
+++ server/controllers/company.go
@@ -7,29 +7,28 @@ import (
 )
 
 func Company(router fiber.Router) {
-
 	company := router.Group("company")
 
 	// Manage Company
 	company.Post("/", handlers.CreateCompany)
 	company.Get("/:id", middleware.CompanyResourceAccessRestriction(handlers.GetCompany))
 	company.Put("/:id", middleware.CompanyResourceAccessRestriction(handlers.UpdateCompany))
-	company.Delete("/:id", handlers.DeleteCompany)
+	company.Delete("/:id", middleware.CompanyResourceAccessRestriction(handlers.DeleteCompany))
 
 	// Manage Company Users
 	company.Get("/:id/user", middleware.CompanyResourceAccessRestriction(handlers.GetUsersByCompany))
-	company.Put("/:id/user", handlers.UpdateCompanyUsers)
-	company.Delete("/:id/user/:userId", handlers.RemoveUserFromCompany)
-	company.Post("/:id/user/invite", handlers.InviteUserToCompany)
-	company.Post("/:id/user/invite/:code", handlers.AddNewUserToCompany)
-	company.Put("/:id/user/invite/:code", handlers.AddExistingUserToCompany)
+	company.Put("/:id/user", middleware.CompanyResourceAccessRestriction(handlers.UpdateCompanyUsers))
+	company.Delete("/:id/user/:userId", middleware.CompanyResourceAccessRestriction(handlers.RemoveUserFromCompany))
+	company.Post("/:id/user/invite", middleware.CompanyResourceAccessRestriction(handlers.InviteUserToCompany))
+	company.Post("/:id/user/invite/:code", middleware.CompanyResourceAccessRestriction(handlers.AddNewUserToCompany))
+	company.Put("/:id/user/invite/:code", middleware.CompanyResourceAccessRestriction(handlers.AddExistingUserToCompany))
 
 	// Manage Company Locations
-	company.Delete("/:id/location", handlers.DeleteCompanyLocation)
+	company.Delete("/:id/location", middleware.CompanyResourceAccessRestriction(handlers.DeleteCompanyLocation))
 
 	// Manage Company Services
 	company.Get("/:id/service", middleware.CompanyResourceAccessRestriction(handlers.GetCompanyServices))
-	company.Post("/:id/service", handlers.CreateCompanyServices)
+	company.Post("/:id/service", middleware.CompanyResourceAccessRestriction(handlers.CreateCompanyServices))
 
 	// Manage Company Invoices
 	company.Get("/:id/invoice", middleware.CompanyResourceAccessRestriction(handlers.GetCompanyInvoices))
